c51501a8e37f787e809227e427072adf544bf914
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-12 21:51:32 +0530

    day 14 uc3 queue

---
+++ day14 learning.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace DataStructuredemo
+{
+    class program
+{
+    static void main(string[] args)
+    {
+        Console.WriteLine("welcome to the Linked List operations");
+        LinkedList List = new LinkedList();
+        List.Add(56);
+        List.Add(30);
+        List.Add(70);
+        List = display();
+        Console.WriteLine("search element" = List.search(30));
+    }
+
+
+}
+
+}
