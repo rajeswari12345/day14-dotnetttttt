ad3dbe64e26b0f23d97de3b5b2c38a2dc6c2a54a
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-12 21:45:02 +0530

    day14 uc4

---
+++ day14 uc4.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace linked_list_day14
+{
+    public class LinkedList
+    {
+
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+
+                    temp.next = node;
+                }
+                Console.WriteLine("{0} inserted into linked list", node.data)}
+        }
+
+        internal void InsertLast(LinkedList, LinkedList, int data)
+        {
+            Node newNode = new Node(data);
+            if (LinkedList.head == null)
+            {
+                newNode.prev = null;
+                LinkedList.head = newNode;
+                return;
+            }
+            Node lastNode = GetLastNode(LinkedList);
+            lastNode.next = newNode;
+            newNode.prev = lastNode;
+        }
