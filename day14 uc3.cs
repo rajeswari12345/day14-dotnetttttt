c0e1b00778440fd172e523c06f7d988008eaafe9
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-12 21:44:32 +0530

    day14 uc3

---
+++ day14 uc3.cs
@@ -0,0 +1,147 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+ 
+// Create Stack Using Linked list
+public class StackUsingLinkedlist
+{
+
+    // A linked list node
+    private class Node
+    {
+        // integer data
+        public int data;
+
+        // reference variable Node type
+        public Node link;
+    }
+
+    // create global top reference variable
+    Node top;
+
+    // Constructor
+    public StackUsingLinkedlist()
+    {
+        this.top = null;
+    }
+
+    // Utility function to add
+    // an element x in the stack
+    // insert at the beginning
+    public void push(int x)
+    {
+        // create new node temp and allocate memory
+        Node temp = new Node();
+
+        // check if stack (heap) is full.
+        // Then inserting an element
+        // would lead to stack overflow
+        if (temp == null)
+        {
+            Console.Write("\nHeap Overflow");
+            return;
+        }
+
+        // initialize data into temp data field
+        temp.data = x;
+
+        // put top reference into temp link
+        temp.link = top;
+
+        // update top reference
+        top = temp;
+    }
+
+    // Utility function to check if
+    // the stack is empty or not
+    public bool isEmpty()
+    {
+        return top == null;
+    }
+
+    // Utility function to return
+    // top element in a stack
+    public int peek()
+    {
+        // check for empty stack
+        if (!isEmpty())
+        {
+            return top.data;
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+            return -1;
+        }
+    }
+
+    // Utility function to pop top element from the stack
+    public void pop() // remove at the beginning
+    {
+        // check for stack underflow
+        if (top == null)
+        {
+            Console.Write("\nStack Underflow");
+            return;
+        }
+
+        // update the top pointer to
+        // point to the next node
+        top = (top).link;
+    }
+
+    public void display()
+    {
+        // check for stack underflow
+        if (top == null)
+        {
+            Console.Write("\nStack Underflow");
+            return;
+        }
+        else
+        {
+            Node temp = top;
+            while (temp != null)
+            {
+
+                // print node data
+                Console.Write("{0}->", temp.data);
+
+                // assign temp link to temp
+                temp = temp.link;
+            }
+        }
+    }
+}
+
+// Driver code
+public class GFG
+{
+    public static void Main(String[] args)
+    {
+        // create Object of Implementing class
+        StackUsingLinkedlist obj = new StackUsingLinkedlist();
+
+        // insert Stack value
+        obj.push(30);
+        obj.push(56);
+        obj.push(70);
+        
+
+        // print Stack elements
+        obj.display();
+
+        // print Top element of Stack
+        Console.Write("\nTop element is {0}\n", obj.peek());
+
+        // Delete top element of Stack
+        obj.pop();
+        obj.pop();
+
+        // print Stack elements
+        obj.display();
+
+        // print Top element of Stack
+        Console.Write("\nTop element is {0}\n", obj.peek());
+    }
+}
