9e9e7c4ed5a718bf268eccb1e1b722fc841143ef
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-12 21:43:25 +0530

    day14 uc1

---
+++ day14 uc1 .cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace DataStructureDemo; 
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+        Consolewriteline("Hello World");
+             list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
